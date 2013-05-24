cdad71fdb548e0ecd9aa9872fc2e975a6865c350
Ethan Soergel <esoergel@gmail.com>
2013-05-24 19:53:18 -0400

    tweaked settings file for heroku

--- settings.py
+++ settings.py
@@ -387,7 +387,7 @@ LOGIN_URL = "/accounts/login/"
 if os.environ.get("RACK_ENV", None) == "production":
     import dj_database_url
     
-    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+    DATABASES = {'default': dj_database_url.config()} # default='postgres://localhost')}
 
     INSTALLED_APPS += ("gunicorn",
                        "storages")
