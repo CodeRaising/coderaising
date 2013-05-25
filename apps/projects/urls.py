e6fb6f01a42714f0ad2c5a6bbcb61d49f8911f00
TJ Keemon <keems21@gmail.com>
2013-05-25 14:18:44 -0400

    used project.slug for project_description urls. updated urls.py to deal with multiple dashes

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -20,19 +20,19 @@ urlpatterns = patterns("",
 	url(r"^propose-project/?$", ProjectProposeView.as_view(), name="project_propose"),
 
 	# project details url: /projects/(id)-(slug)/
-	url(r"^(?P<pk>\d+)-?[\w|\s]*/?$", ProjectDetailView.as_view(), name="project_detail"),
+	url(r"^(?P<pk>\d+)-?[\w|\s|-]*/?$", ProjectDetailView.as_view(), name="project_detail"),
 	
 	# project edit page url: /projects/(id)-(slug)/edit-project
-	url(r"^(?P<pk>\d+)-?[\w|\s]*/edit-project/$", ProjectEditView.as_view(), name="project_edit"),
+	url(r"^(?P<pk>\d+)-?[\w|\s|-]*/edit-project/$", ProjectEditView.as_view(), name="project_edit"),
 
 	# project users list view url: /projects/(id)-(slug)/users
-	url(r"^(?P<pk>\d+)-?[\w|\s]*/users$", ProjectUsersView.as_view(), name="project_users"),
+	url(r"^(?P<pk>\d+)-?[\w|\s|-]*/users$", ProjectUsersView.as_view(), name="project_users"),
 
 	# apply to project url: /projects/(id)-(slug)/apply-to-project
-	url(r"^(?P<project_id>\d+)-?[\w]*/apply-to-project$", ProjectApplyView.as_view(), name="project_apply"),
+	url(r"^(?P<project_id>\d+)-?[\w|\s|-]*/apply-to-project$", ProjectApplyView.as_view(), name="project_apply"),
 
 	# view applicants url: /projects/(id)-(slug)/view-applicants
-	url(r"^(?P<project_id>\d+)-?[\w]*/view-applicants$", ProjectApplicantsView.as_view(), name="project_applicants"),
+#url(r"^(?P<project_id>\d+)-?[\w]*/view-applicants$", ProjectApplicantsView.as_view(), name="project_applicants"),
 
 	# project details for /projects/(id)-(slug)/view-applicants/accept-applicants
 #url(r"^(?P<project_id>\d+)-[\w]+/view-applicants/accept-applicants$", views.ProjectAcceptApplicantsView.as_view(), name="project_accept_applicants"),
