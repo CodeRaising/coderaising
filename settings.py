57ee22ff5c6e84e5b2d3f95f9415c5366e5cccd8
Ethan Soergel <esoergel@gmail.com>
2013-05-25 16:00:45 -0400

    fuckin twitter...

--- settings.py
+++ settings.py
@@ -6,7 +6,8 @@ import dj_database_url
 
 SITE_TAGLINE = "a continuous code jam..."
 SITE_TITLE = "CodeRaising"
-TWITTER_DEFAULT_QUERY = "CodeRaising"
+TWITTER_DEFAULT_QUERY = "@CodeRaising"
+TWITTER_DEFAULT_QUERY_TYPE = "user"
 
 # The following settings are already defined with default values in
 # the ``defaults.py`` module within each of Mezzanine's apps, but are
