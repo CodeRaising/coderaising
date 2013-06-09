a401a6bb8c5b40b661b0a461e3207f71423c435c
Brian Yee <yee.brianj@gmail.com>
2013-06-09 13:26:05 -0400

    added tagging to userprofile model

--- apps/userprofile/models.py
+++ apps/userprofile/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from taggit.models import GenericTaggedItemBase, TagBase
+
+
+class LearnTag(TagBase):
+    pass
+
+
+class LearnTaggedItem(GenericTaggedItemBase):
+    tag = models.ForeignKey(LearnTag, related_name="learn")
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField("auth.User", editable=False)
-    signup_date = models.DateTimeField("Signup date", auto_now_add = True)
+    signup_date = models.DateTimeField("Signup date", auto_now_add=True)
     bio = models.TextField()
-    tech_to_learn = models.TextField("Technologies I want to learn more about")
-    tech_i_know = models.TextField("Technologies I know")
+    learn = TaggableManager(verbose_name="Tech I want to learn", through=LearnTaggedItem, blank=True)
+    skills = TaggableManager(verbose_name="Tech I know", blank=True)
 
     def __unicode__(self):
         return self.user.username
